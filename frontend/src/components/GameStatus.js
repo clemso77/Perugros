14706e5b8e445c78d7d688af2ffa8dec18637e4b
Clement <clement-martins@live.fr>
2024-10-05 17:05:43 +0200

    ok

--- frontend/src/components/GameStatus.js
+++ frontend/src/components/GameStatus.js
@@ -18,19 +18,6 @@ const GameStatus = ({ group,  socket, diceBetCount, diceBetValue, currentTurnPla
     return (
         <>
             <div className='message-container'>
-                {group && !currentTurnPlayer && (
-                    <div className='info'>ID de la partie : {group}
-                       <img src='/texture/icon/copy.png' alt='' onClick={() => {
-                        navigator.clipboard.writeText(group) // Passer directement group sans accolades
-                            .then(() => {
-                                alert("Copied the text: " + group); // Enlever les accolades ici aussi
-                            })
-                            .catch(err => {
-                                console.error("Failed to copy: ", err);
-                            });
-                    }} />
-                    </div>
-                )}
                 {currentTurnPlayer && <p className="info">C'est au tour de : {currentTurnPlayer}</p>}
                 {diceBetCount && diceBetValue && <p>Parie en cour: {diceBetCount} * {diceBetValue}</p>}
                 {err && <p className="error">{err}</p>}
